ed98c7f9e58cafd2214b8ada9bf2fe3384d83a20
rajputgovind <govindrajput7649@gmail.com>
2023-04-03 14:16:10 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 // app.use(cors())
 dotenv.config({path:'./config.env'})
 
-const PORT  = process.env.PORT
+const PORT  = process.env.PORT || 5000
 app.use(express.json())
 
 app.use(require('./src/routers/UserRouter'))
